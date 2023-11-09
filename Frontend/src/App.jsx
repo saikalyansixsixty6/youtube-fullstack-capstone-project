7fcda87f5d177b2f257319c3bfed04cbda76279f
Saikalyan <saikalyan2406@gmail.com>
2023-11-09 13:22:17 +0530

    vite : Frontend setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from './Components/Navbar/Navbar'
+import './App.css'
+
+function App() {
+  
+  
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import "./Navbar.css"
+import "./YouTube-Logo.wine.svg"
+const  Navbar = ()=> {
+  return (
+    <div className="Container_navbar">
+      <div className="logo">
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
